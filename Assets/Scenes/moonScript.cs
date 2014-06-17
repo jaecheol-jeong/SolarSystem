3e76fe55e07f24f4ae5e2ba78de1b6ba83ff79f2
JEONG JAE CHEOL <ranos94+git@gmail.com>
2014-06-17 23:25:45 +0900

    add source

---
+++ Assets/Scenes/moonScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moonScript : MonoBehaviour {
+
+	public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate( 0, 2 * speed * Time.deltaTime, 0);
+	}
+}
